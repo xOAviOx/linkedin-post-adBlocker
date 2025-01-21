7284bb5234551af93c78678864e5335fdca08c6b
xOAviOx <avishuklacode@gmail.com>
2025-01-21 18:59:29 +0530

    linkedin.js

--- linkedin.js
+++ linkedin.js
@@ -8,7 +8,25 @@ function removeAds() {
       //get div that wraps around the entire ad
       let card = spans[i].closest(".fie-impression-container");
 
-      
+      if (card === null) {
+        let j = 0;
+        card = spans[i];
+
+        while (j < 6) {
+          card.card.parentNode;
+          ++j;
+        }
+      }
+
+      //making the ad/post dissapear
+      card.setAttribute("style", "display:none !important");
     }
   }
 }
+removeAds();
+
+//ensures ads / posts will be removed as the user scrolls
+
+setInterval(function () {
+  removeAds();
+}, 100);
