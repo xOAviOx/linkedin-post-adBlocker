f33cdfb14bedd87713745641d3262b78d43b91e1
xOAviOx <avishuklacode@gmail.com>
2025-01-21 18:45:12 +0530

    background js

--- background.js
+++ background.js
@@ -10,10 +10,38 @@ chrome.webNavigation.onCommited.addListener(function (tab) {
         .replace("http://", "")
         .replace("www.", "");
 
-      let domain = parsedUrl.slice(
-        0,
-        parsedUrl.indexOf("/" == -1 ? parsedUrl.length : parsedUrl.indexOf("?"))
-      );
+      let domain = parsedUrl
+        .slice(
+          0,
+          parsedUrl.indexOf("/") == -1
+            ? parsedUrl.length
+            : parsedUrl.indexOf("/")
+        )
+        .slice(
+          0,
+          parsedUrl.indexOf("?") == -1
+            ? parsedUrl.length
+            : parsedUrl.indexOf("?")
+        );
+
+      try {
+        if (domain.length < 1 || domain === null || domain === undefined) {
+          return;
+        } else if (domain == "linkedin.com") {
+          runLinkedinScript();
+          return;
+        }
+      } catch (err) {
+        throw err;
+      }
     });
   }
 });
+
+function runLinkedinScript() {
+  chrome.tabs.executeScript({
+    file: "linkedin.js",
+  });
+
+  return true;
+}
